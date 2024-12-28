c044b07550322587e11cd42c848e27977934a75e
Павел Старостин <PaStarostin@alabuga.local>
2024-12-28 15:23:33 +0300

    Starostin/backend

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,1 +1,13 @@
 from django.urls import path
+
+from .views import BookListView, BookCreateView, BookDeleteView, BookUpdateView, BookDetailView, buy, orders
+
+urlpatterns = [
+    path('index', BookListView.as_view(), name='book-list'),
+    path('create', BookCreateView.as_view(), name='book-create'),
+    path('buy', buy, name='buy'),
+    path('orders', orders, name='orders'),
+    path('delete/<int:pk>', BookDeleteView.as_view(), name='book-delete'),
+    path('update/<int:pk>', BookUpdateView.as_view(), name='book-update'),
+    path('detail/<int:pk>', BookDetailView.as_view(), name='book-detail'),
+]
